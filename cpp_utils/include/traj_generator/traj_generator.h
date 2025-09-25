012a9689b05ed6c8c15bdded587a42c3334a7f14
sosoeeee <809953371@qq.com>
2025-09-25 12:16:23 +0000

    generator human path withour local noises

--- cpp_utils/include/traj_generator/traj_generator.h
+++ cpp_utils/include/traj_generator/traj_generator.h
@@ -35,6 +35,7 @@ public:
     std::vector<Point> sampleTrajLoop(Point start, Point end);
     std::vector<Point> sampleDistinctHomotopyTrajs(Point start, Point end);
     std::vector<Point> sampleDistinctHomotopyTrajsLoop(Point start, Point end);
+    std::pair<std::vector<Point>, std::vector<Point>> sampleTrajLoopWithInit(Point start, Point end);
 
     // for visualization
     std::vector<PoseSE2> getInitPlan() const {return init_plan_;}
@@ -58,6 +59,7 @@ private:
     // void getNearestNode(Point p, int &node_id);
     void updateCorridor();
     void updateViaPoints();
+    void updateViaPointsNoSample();
     void updateTrajectory();
 
     std::shared_ptr<Costmap2D> costmap_;
